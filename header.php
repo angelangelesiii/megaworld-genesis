909cf5681f9d536a898d85fbb643e40041dcf90b
Angel Angeles <angelangelesiii@gmail.com>
2018-08-03 14:54:21 +0800

    Initial header styling

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 
 	<header id="siteheader" class="site-header <?php echo $headerClasses; ?>">
 		<nav id="sitenav" class="main-site-navigation">
-			<div class="wrapper-big">
+			<div class="wrapper-medium">
 				<div class="nav-container grid-x">
 					<div class="logo-container cell shrink">
 						<img src="<?php echo get_template_directory_uri().'/images/logos/mw_logo_white.png' ?>" alt="Megaworld">
