a05049481bdaa631e9fb6e3265b25244c25d9790
Angel Angeles <angelangelesiii@gmail.com>
2018-08-09 13:04:42 +0800

    Updated blog page files

--- index.php
+++ index.php
@@ -15,45 +15,82 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area blog-area">
 		<main id="main" class="site-main">
+			
+			<div class="wrapper-medium">
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				<header class="page-header">
+					<h1 class="page-title">The Blog</h1>
 				</header>
-				<?php
-			endif;
+				
+				<div class="grid-x">
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+					<div class="main-column cell large-9  medium-8 small-12">
+						<?php
+						if ( have_posts() ) : ?>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+							
+							<div class="blog-posts">
 
-			endwhile;
+							<?php while (have_posts()): the_post(); ?>
 
-			the_posts_navigation();
+								<article class="article <?php echo get_post_type(); ?>">
+									
 
-		else :
+									<?php 
+									$thumbBool = '';
+									if(has_post_thumbnail()): 
+									$thumbBool = 'thumb-true';
+									?>
+									<div class="date thumb-date"><?php echo get_the_date(); ?></div>
+									<a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url( $post->ID, 'bg_medium' );  ?>" alt="<?php the_title(); ?>" class="featured-image"></a>
+									<?php else: ?>
+									<div class="date-container">
+										<div class="date"><?php echo get_the_date(); ?></div>
+									</div>
+									<?php endif; ?>
 
-			get_template_part( 'template-parts/content', 'none' );
+									<div class="text-contents <?php echo $thumbBool; ?>">
+										<h2 class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+										<div class="excerpt-container">
+											<?php the_excerpt(); ?>
+										</div>
+										<a href="<?php the_permalink(); ?>" class="btn white read-more-btn">Read More</a>
+									</div>
+								</article>
 
-		endif;
-		?>
+							<?php endwhile; ?>
+
+							</div>
+
+							<div class="pagination-links">
+							<?php the_posts_pagination( array(
+								'mid_size' => 2,
+								'prev_text' => __( 'Previous', 'textdomain' ),
+								'next_text' => __( 'Next', 'textdomain' ),
+							) ); ?>
+							</div>
+						
+						<?php
+						else :
+				
+							get_template_part( 'template-parts/content', 'none' );
+				
+						endif;
+						?>
+					</div>
+
+					<div class="sidebar-column cell large-3 medium-4 small-12">
+						<?php get_sidebar(); ?>
+					</div>
+
+				</div>
+
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
